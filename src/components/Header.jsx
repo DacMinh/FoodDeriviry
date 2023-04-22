d59d9f7e29c54f051d44eb7fe3cff1de48d6dd19
Minh <phamdacminh38@gmail.com>
2023-04-22 07:21:33 +0700

    uppdate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "./imgs/logo.png";
-import { MdShoppingBasket } from "react-icons/md";
+import { MdShoppingBasket,MdAdd,MdLogout } from "react-icons/md";
 import Avatar from "./imgs/avatar.png";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase.config";
 import { useStateValue } from "../context/StateProvider";
-import {actionType} from "../context/reducer"
+import { actionType } from "../context/reducer";
 
 const Header = () => {
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
-  const[{user},dispatch] = useStateValue()
+  const [{ user }, dispatch] = useStateValue();
   const login = async () => {
-    const {user:{refreshToken,providerData}} = await signInWithPopup(firebaseAuth, provider);
-    dispatch({
-      type : actionType.SET_USER,
-      user : providerData [0],
-    })
-
+    if (!user) {
+      const {
+        user: { refreshToken, providerData },
+      } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      });
+      localStorage.setItem("user", JSON.stringify(providerData[0]));
+    }
   };
   return (
     <header className="fixed z-50 w-screen p-6 px-16">
@@ -61,6 +66,13 @@ const Header = () => {
             alt="avatar"
             onClick={login}
           />
+          <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 left-0 right-0">
+            {user&& user.email === "phamdacminh2@gmail.com" && (
+            <Link to={"/createItem"}>
+            <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100  transition-all duration-100 ease-in-out text-textColor text-base">Món mới <MdAdd/></p> </Link>)}
+            
+            <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100  transition-all duration-100 ease-in-out text-textColor text-base">Đăng xuất <MdLogout/></p>
+          </div>
         </div>
       </div>
 
