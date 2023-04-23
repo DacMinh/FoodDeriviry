c50eba61e1990d804c451806a03b71779d977c04
Minh <phamdacminh38@gmail.com>
2023-04-23 11:12:22 +0700

    updating

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Logo from "./imgs/logo.png";
+import Logo from "../imgs/logo.png";
 import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
-import Avatar from "./imgs/avatar.png";
+import Avatar from "../imgs/avatar.png";
 import { motion } from "framer-motion";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase.config";
@@ -28,8 +28,21 @@ const Header = () => {
       setIsMenu(!isMenu);
     }
   };
+const logout = () =>{
+  setIsMenu (false)
+  localStorage.clear()
+
+  dispatch({
+    type: actionType.SET_USER,
+    user:null,
+  });
+
+};
+
+
+
   return (
-    <header className="fixed z-50 w-screen p-6 px-16">
+    <header className="fixed z-50 w-screen p-6 px-4 md:p-6 ">
       {/* cho máy tính và tablet */}
       <div className="hidden md:flex w-full h-full  items-center justify-between ">
         <Link to={"/"} className="flex items-center gap-2 flex-grow">
@@ -88,7 +101,9 @@ const Header = () => {
                 </Link>
               )}
 
-              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100  transition-all duration-100 ease-in-out text-textColor text-base">
+              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100  transition-all duration-100 ease-in-out text-textColor text-base"
+              
+              onClick={logout}>
                 Đăng xuất <MdLogout />
               </p>
             </motion.div>
@@ -97,7 +112,86 @@ const Header = () => {
       </div>
 
       {/* cho điện thoai */}
-      <div className="flex md:hidden h-full">
+      <div className="flex md:hidden h-full w-full justify-between items-center">
+      <Link to={"/"} className="flex items-center gap-2 flex-grow">
+          <img src={Logo} className="w-8 object-cover" alt="logo" />
+          <p className=" text-headingColor text-xl font-bold"> Gà Quay </p>
+        </Link>
+
+        
+        <div className="relative flex items-center justify-center ">
+          <MdShoppingBasket className="text-textColor text-2xl  cursor-pointer" />
+          <div className="w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center absolute -top-3 -right-3 ">
+            <p className="text-xs text-white font-semibold"> 2</p>
+          </div>
+        </div>
+    
+
+
+
+        <div className="relative">
+          <motion.img
+            whileTap={{ scale: 0.6 }}
+            src={user ? user.photoURL : Avatar}
+            className="w-10 min-8-[40px] h-10 min-h-[40px] drop-shadow-xl m-5  cursor-pointer rounded-full"
+            alt="avatar"
+            onClick={login}
+          />
+          {isMenu && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.6 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.6 }}
+              className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-17 right-5"
+            >
+              {user && user.email === "phamdacminh2@gmail.com" && (
+                <Link to={"/createItem"}>
+                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100  transition-all duration-100 ease-in-out text-textColor text-base">
+                    Món mới <MdAdd />
+                  </p>{" "}
+                </Link>
+              )}
+                        <ul
+  
+            className="flex  flex-col "
+          >
+            <li className="text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out hover:bg-slate-100 px-4 py-2 ">
+              Trang chủ
+            </li>
+            <li className="text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out hover:bg-slate-100 px-4 py-2">
+              Danh sách
+            </li>
+            <li className="text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out hover:bg-slate-100 px-4 py-2">
+              Về chúng tôi
+            </li>
+            <li className="text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out hover:bg-slate-100 px-4 py-2
+            
+            
+            ">
+              Dịch vụ
+            </li>
+          </ul>
+
+
+
+
+
+
+
+
+
+
+
+
+              
+
+              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer bg-slate-200 hover:bg-slate-300  transition-all duration-100 ease-in-out text-textColor text-base
+              m-2 p-2 rounded-md shadow-md" onClick={logout}>
+               Đăng xuất<MdLogout />
+              </p>
+            </motion.div>
+          )}
+        </div>
 
 
 
---
+++ src/ultis/data.js
@@ -0,0 +1,29 @@
+import I1 from "../imgs/i1.png";
+import F1 from "../imgs/f1.png";
+import C3 from "../imgs/c3.png";
+import Fi1 from "../imgs/fi1.png";
+
+export const heroData = [
+  {
+    id: 1,
+    name: "Kem Socola",
+    decp: "Chocolate & vanilla",
+    price: "25.000",
+    imageSrc: I1,
+  },
+  { id: 2, name: "Dâu tươi", decp: "Dâu tươi", price: "28.000", imageSrc: F1 },
+  {
+    id: 3,
+    name: "Cơm gà",
+    decp: "Gà,cơm,rau ăn kèm",
+    price: "30.000",
+    imageSrc: C3,
+  },
+  {
+    id: 4,
+    name: "Hải Sản",
+    decp: "Sò,hến, ăn kèm nước mắm",
+    price: "50.000",
+    imageSrc: Fi1,
+  },
+];
