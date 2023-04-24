77f4e5168b7087ba0e66cff1e9deeff98cb44ff5
Minh <phamdacminh38@gmail.com>
2023-04-24 22:40:55 +0700

    updating

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -3,14 +3,17 @@ import HomeContainer from "./HomeContainer";
 import {motion} from 'framer-motion';
 import {MdChevronLeft,MdChevronRight} from 'react-icons/md'
 import RowContainer from "./RowContainer";
+import { useStateValue } from "../context/StateProvider";
 
-const MainContainer = () => {
+const MainContainer = ({flag,data}) => {
+
+  const [{foodItem},dispatch] = useStateValue()
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center">
       <HomeContainer />
       <section className="w-full my-6">
         <div className="w-full  flex items-center justify-between">
-          <p className="text-2xl font-semibold capotalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
+          <p className="text-2xl font-semibold capitalize  text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
             Trái cây tươi và tốt cho sức khỏe của chúng tôi
           </p>
           <div className="hidden md:flex gap-3 items-center">
@@ -20,7 +23,7 @@ const MainContainer = () => {
 
           </div>
         </div>
-        <RowContainer flag={true}/>
+        <RowContainer flag={true} data={foodItem?.filter((n)=>n.category=== 'Nước ép')}/>
       </section>
     </div>
   );
--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,21 +1,48 @@
-import React from 'react'
-import {MdShoppingBasket} from 'react-icons/md';
-import {motion} from 'framer-motion';
-const RowContainer = ({flag}) => {
+import React from "react";
+import { MdShoppingBasket } from "react-icons/md";
+import { motion } from "framer-motion";
+const RowContainer = ({ flag, data }) => {
   return (
-    <div className={`w-full ${flag ? 'overflow-scroll': 'overflow-x-hidden' }`}>
-        
-        <div className='w-300 md:w-225  h-auto my-12 backdrop-blur-lg '>
-            <div className='w-full flex items-center justify-between'>
-                <img src='https://firebasestorage.googleapis.com/v0/b/restaurantapp-c95ea.appspot.com/o/imgaes%2F1682302466627-i7.png?alt=media&token=820c8725-4b8d-4ed6-8822-6c0e84cfe636'className='w-40 -mt-8'  alt='' />
-                <motion.div whileTap={{scale:0.75}} className='w-8 h-8 rounded-full bg-pink-600 flex items-center justify-center cursor-pointer hover:shadow-md'>
-                    <MdShoppingBasket className='text-white'/>
-
-                </motion.div>
+    <div
+      className={`w-full my-12 ${
+        flag ? "overflow-scroll" : "overflow-x-hidden"
+      }`}
+    >
+      {data &&
+        data.map((item) => (
+          <div
+            key={item.id}
+            className="w-300 md:w-350   h-auto bg-cardOverlay rounded-lg my-12 backdrop-blur-lg p-2 hover:drop-shadow-xl "
+          >
+            <div className="w-full flex items-center justify-between">
+              <motion.img
+                whileTap={{ scale: 1.2 }}
+                src="https://firebasestorage.googleapis.com/v0/b/restaurantapp-c95ea.appspot.com/o/imgaes%2F1682302466627-i7.png?alt=media&token=820c8725-4b8d-4ed6-8822-6c0e84cfe636"
+                className="w-40 -mt-8 drop-shadow-2xl"
+                alt=""
+              />
+              <motion.div
+                whileTap={{ scale: 0.75 }}
+                className="w-8 h-8 rounded-full bg-pink-600 flex items-center justify-center cursor-pointer hover:shadow-md"
+              >
+                <MdShoppingBasket className="text-white" />
+              </motion.div>
             </div>
-        </div>
-         </div>
-  )
-}
+            <div className="w-full flex flex-col gap-4 items-end justify-end">
+              <p className="text-textColor font-semibold text-base md:text-lg">
+                Kem dâu
+              </p>
+              <p className="mt-1 text-sm text-gray-500">45 Calories</p>
+              <div className="flex items-center gap-8">
+                <p className="text-lg text-headingColor font-semibold">
+                  25000 <span className="text-sm text-red-500">VND</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
 
-export default RowContainer
+export default RowContainer;
