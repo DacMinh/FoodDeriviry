9e532b3b688eebc45945a5b5c88624f0386f3269
Minh <phamdacminh38@gmail.com>
2023-04-29 22:21:22 +0700

    updating

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,34 +1,51 @@
-import React, { useRef, useState,useEffect  } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import HomeContainer from "./HomeContainer";
-import {motion} from 'framer-motion';
-import {MdChevronLeft,MdChevronRight} from 'react-icons/md'
+import { motion } from "framer-motion";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import RowContainer from "./RowContainer";
 import { useStateValue } from "../context/StateProvider";
+import MenuContainer from "./MenuContainer";
 
 const MainContainer = () => {
-  const [scrollValue,setScrollValue] =useState(0)
-  const [{foodItems},dispatch] = useStateValue()
-useEffect (() =>{}, [scrollValue] )
-  
+  const [scrollValue, setScrollValue] = useState(0);
+  const [{ foodItems }, dispatch] = useStateValue();
+  useEffect(() => {}, [scrollValue]);
+
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center">
       <HomeContainer />
       <section className="w-full my-6">
         <div className="w-full  flex items-center justify-between">
           <p className="text-2xl font-semibold capitalize  text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
-            Trái cây tươi và tốt cho sức khỏe của chúng mình
+            Trái cây tươi 
           </p>
           <div className="hidden md:flex gap-3 items-center">
-            <motion.div whileTap={{scale:0.75}} className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center" onClick={()=> setScrollValue(-200)}> <MdChevronLeft className="text-lg text-white"  /></motion.div>
-            <motion.div whileTap={{scale:0.75}} className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"  onClick={()=> setScrollValue(200)}> <MdChevronRight className="text-lg text-white"  /></motion.div>
-           
-
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
+              onClick={() => setScrollValue(-200)}
+            >
+              {" "}
+              <MdChevronLeft className="text-lg text-white" />
+            </motion.div>
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
+              onClick={() => setScrollValue(200)}
+            >
+              {" "}
+              <MdChevronRight className="text-lg text-white" />
+            </motion.div>
           </div>
         </div>
-        <RowContainer scrollValue = {scrollValue}  flag={true} data={foodItems?.filter((n)=>n.category ==='Nước ép')}/>
+        <RowContainer
+          scrollValue={scrollValue}
+          flag={true}
+          data={foodItems?.filter((n) => n.category === "Nước ép")}
+        />
       </section>
-      <section className='w-full my-6'>
-        </section>
+      <section className="w-full my-6"></section>
+      <MenuContainer />
     </div>
   );
 };
